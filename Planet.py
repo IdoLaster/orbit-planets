9041641237e6f2aba31a337ffd947dedb962a445
Ido <laster.ido@gmail.com>
2020-09-23 15:24:55 +0300

    Attempt to add multiple plants support
    Currently it's seems like the planets are moving in the wrong direction

--- Planet.py
+++ Planet.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Planet():
-    G = 10000 # Gravitional constant
+    G = 100 # Gravitional constant
     def __init__(self, name, color, radius, mass, pos):
         self.name = name
         self.color = color
@@ -16,24 +16,18 @@ class Planet():
         self.velocty = self.velocty + self.acceleration
         
         self.pos = self.pos + self.velocty
-        print(f"Earth {self.pos=}")
 
         self.acceleration = self.acceleration * 0
 
     def get_gravity_force(self, planet_other):
         dir = self.pos - planet_other.pos
-        print(f"{dir=}")
         distance = np.linalg.norm(dir) # In this case the distance is equals to the vector magnitue
         dir = dir / np.linalg.norm(dir)
-        print(f"{dir=}")
-        print(f"{distance=}")
         strength = (self.G * self.mass * planet_other.mass) / distance ** 2
-        print(f"{strength=}")
         force = dir * strength
-        print(f"{force=}")
+
         return force
 
     def apply_force(self, force):
         f = force / self.mass
         self.acceleration = self.acceleration + f
-        print(f"{self.acceleration=}")
