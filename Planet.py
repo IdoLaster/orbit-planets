e20401a1c103a1d910ffad55af5b40d10373bc42
Ido <laster.ido@gmail.com>
2020-09-24 16:15:10 +0300

    Add docstring and clean code

--- Planet.py
+++ Planet.py
@@ -3,22 +3,44 @@ import math
 
 class Planet():
 	G = 1 # Gravitional constant
-	def __init__(self, name, color, radius, mass, pos):
+
+	def __init__(self, name, color, radius, mass, pos, velocity=(0,0)):
+		"""
+		Inits a new planet object.
+		The name, color, radius not effect the calculations, they are for displaying reasons.
+
+		Args:
+			name (String): The name of the planet.
+			color (tuple): (R, G, B) values of the planet color
+			radius (Integer): The radius of the planet.
+			mass (Float): The mass of the planet.
+			pos (tuple): (Px, Py) the position of the planet
+		"""
 		self.name = name
 		self.color = color
-		self.radius = radius # This is not used in any of the calculation, but for the graphics
+		self.radius = radius 
+
 		self.mass = mass
 		self.pos = np.array(pos)
+		self.velocity = np.array(velocity)
 
-		self.velocty = np.array((0,0))
-
-		self.acceleration = np.array((0,0))
 
 	def update(self):
-		self.pos = self.pos + self.velocty
-		#self.velocty = self.velocty * 0
+		"""
+		Updates self position by the current velocity
+		"""
+		self.pos = self.pos + self.velocity
 
 	def get_gravity_force(self, planet_other):
+		"""
+		Calculates the graivitonal force that planet_other effects self 
+
+		Args:
+			planet_other (Planet): The planet we are calculating the graivtonal force from.
+
+		Returns:
+			np.array: [Fx, Fy] The graivitonal force that applies to self.
+		"""
 		direction = (self.pos - planet_other.pos ) * -1
 		
 		distance = np.linalg.norm(direction)
@@ -31,5 +53,11 @@ class Planet():
 		return force # [fx, fy]
 
 	def apply_force(self, force):
+		"""
+		Applies force to self
+
+		Args:
+			force (np.array): [Fx, Fy] The force to apply to self.
+		"""
 		f = force / self.mass
-		self.velocty = self.velocty + f
+		self.velocity = self.velocity + f
--- main.py
+++ main.py
@@ -6,30 +6,40 @@ import sys
 
 from Planet import Planet
 
-width, height = 1920, 1080
-SCALE = 10000
+width, height = 1920, 1080 # PyGame window size
+SCALE = 10000 # Scale, still need to figure it out
+
 def create_planets():
+	"""
+	This functions creates mock planets.
+
+	Returns:
+		list: List of mock planets.
+	"""
 	planets = []
-	earth = Planet("Earth", (0, 0, 255), 10, 1, (width // 2, 200))
-	earth.velocty = np.array((1,0))
+
+	earth = Planet("Earth", (0, 0, 255), 10, 1, (width // 2, 200), velocity=(1,0))
 	sun = Planet("Sun", (255, 255, 0), int(696340 // SCALE), 1000, (width // 2, height // 2))
-	mars = Planet("MARS", (255, 0, 0), int(300000 // SCALE), 10, (width // 3, height // 2))
-	mars.velocty = np.array((0, 1))
+	mars = Planet("MARS", (255, 0, 0), int(300000 // SCALE), 10, (width // 3, height // 2), velocity=(0,1))
+
 	planets.append(earth)
 	planets.append(mars)
 	planets.append(sun)
+
 	return planets
 
 def main():
+	# Init PyGame
 	pygame.init()
 	
 	fps = 60
 	fpsClock = pygame.time.Clock()
-	font = pygame.font.SysFont("Arial", 32)
-	
-	
 	screen = pygame.display.set_mode((width, height))
+
+	font = pygame.font.SysFont("Arial", 32)
+
 	planets = create_planets()
+
 	# Game loop.
 	while True:
 		screen.fill((0, 0, 0))
@@ -39,7 +49,13 @@ def main():
 				pygame.quit()
 				sys.exit()
 		
+		# Calculate force and apply force.
 		for planet in planets:
+			'''
+			We are removing the current iterator planet since there's is no force 
+			by a planet on himself (this is probably wrong, but I am not very smart)
+			and it will lead to divion by 0.
+			'''
 			tmp_planets = planets.copy()
 			tmp_planets.remove(planet)
 
@@ -48,13 +64,13 @@ def main():
 				planet.apply_force(force)
 				planet.update()
 				
-		# Draw.
-		
+		# Draw all planets and relative labels (labels are temporary).
 		for i, planet in enumerate(planets):
 			pygame.draw.circle(screen, planet.color, planet.pos.astype(int), planet.radius)
 			textsurf = font.render(f"{planet.name}: {planet.pos[0], planet.pos[1]}", False, (255,255,255))
 			screen.blit(textsurf, (0, 34 * i))
 
+		# Update screen.
 		pygame.display.flip()
 		fpsClock.tick(fps)
 
