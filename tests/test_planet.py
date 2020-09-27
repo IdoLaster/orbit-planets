08eedf7f0217408f5e940ca498a56b674439a922
Ido <laster.ido@gmail.com>
2020-09-27 14:57:08 +0300

    Add setup function and a None test for apply_force

--- tests/test_planet.py
+++ tests/test_planet.py
@@ -9,6 +9,11 @@ class TestPlanet(unittest.TestCase):
 	This class contains few basic tests for the planet class.
 	"""
 
+	def setUp(self):
+		self.mock_planet1 = Planet("Test1", (0,0,0), 5, 1, (0,0))
+		self.mock_planet2 = Planet("Test2", (0,0,0), 5, 1, (0,0))
+
+
 	def test_get_gravity_force_calc(self):
 		"""
 		A simple test to make sure we are calculating correctly the gravitional force
@@ -19,8 +24,9 @@ class TestPlanet(unittest.TestCase):
 		F = (G * m1 * m2) / d ** 2
 		Direction = (pos1 - pos2 / d) * -1
 		"""
-		mock_planet1 = Planet("Test1", (0,0,0), 5, 1, (0,0))
-		mock_planet2 = Planet("Test1", (0,0,0), 5, 10, (0,5))
+		self.mock_planet2.pos = np.array((0,5))
+		self.mock_planet2.mass = 10
+
 		"""
 		In this case we set G = 1
 		d = 5
@@ -34,7 +40,7 @@ class TestPlanet(unittest.TestCase):
 		so final force should be:
 		F = (0, 1) * 0.4 = (0, 0.4)
 		"""
-		force = mock_planet1.get_gravity_force(mock_planet2)
+		force = self.mock_planet1.get_gravity_force(self.mock_planet2)
 		fx = force[0]
 		fy = force[1]
 		self.assertAlmostEqual(fx, 0)
@@ -45,10 +51,9 @@ class TestPlanet(unittest.TestCase):
 		A simple test to see if the right excpetions rasies
 		for spesific paramters
 		"""
-		mock_planet1 = Planet("Test1", (0,0,0), 5, 1, (0,0))
 
-		self.assertRaises(ValueError, mock_planet1.get_gravity_force, None),
-		self.assertRaises(ZeroDivisionError, mock_planet1.get_gravity_force, mock_planet1)
+		self.assertRaises(ValueError, self.mock_planet1.get_gravity_force, None),
+		self.assertRaises(ZeroDivisionError, self.mock_planet1.get_gravity_force, self.mock_planet1)
 
 	def test_apply_force_calc(self):
 		"""
@@ -59,15 +64,22 @@ class TestPlanet(unittest.TestCase):
 		inital_velocity = np.array((0,0))
 		expected_vx, expected_vy = inital_velocity + (mock_force / mock_mass)
 
-		
-		mock_planet1 = Planet("Test1", (0,0,0), 5, mock_mass, (0,0), velocity=inital_velocity)
-		mock_planet1.apply_force(mock_force)
+		self.mock_planet1.mass = mock_mass
+		self.mock_planet1.velocity = inital_velocity
+		self.mock_planet1.apply_force(mock_force)
 
-		calc_vx, calc_vy = mock_planet1.velocity
+		calc_vx, calc_vy = self.mock_planet1.velocity
 
 		self.assertAlmostEqual(expected_vx, calc_vx)
 		self.assertAlmostEqual(expected_vy, calc_vy)
 	
+	def test_apply_force_rasies(self):
+		"""
+		A test to make sure the right exception rasies
+		"""
+
+		self.assertRaises(ValueError, self.mock_planet1.apply_force, None)
+
 	def test_update(self):
 		"""
 		A test to make sure we are updating the planet position
@@ -75,11 +87,14 @@ class TestPlanet(unittest.TestCase):
 		"""
 		mock_pos = np.array((0,0))
 		mock_inital_velocity = np.array((0,5))
-		mock_planet = Planet("Test1", (0,0,0), 1, 1, mock_pos, velocity=mock_inital_velocity)
+
+		self.mock_planet1.mass = 1
+		self.mock_planet1.pos = mock_pos
+		self.mock_planet1.velocity = mock_inital_velocity
 
 		expected_px, expected_py = mock_pos + mock_inital_velocity
-		mock_planet.update()
-		calc_px, calc_py = mock_planet.pos
+		self.mock_planet1.update()
+		calc_px, calc_py = self.mock_planet1.pos
 
 		self.assertAlmostEqual(expected_px, calc_px)
 		self.assertAlmostEqual(expected_py, calc_py) 
