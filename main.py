12b32bf5beebb6309b585ebc41778f72ca6d2904
Ido <laster.ido@gmail.com>
2020-09-30 15:38:54 +0300

    Add documentations and clean code

--- main.py
+++ main.py
@@ -6,10 +6,21 @@ import sys
 
 from Planet import Planet
 
-width, height = 1280, 720 # PyGame window size
 SCALE = 10000 # Scale, still need to figure it out
 
-LEFT = 1
+#PyGame constant
+LEFT = 1 # PyGame left mouse bvutton id
+width, height = 1280, 720 # PyGame window size
+HIGHLIGHTED_PLANET_CIRCLE_OFFSET = 5
+FONT_SIZE = 32
+
+# PyGame colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
 
 def create_planets():
 	"""
@@ -20,9 +31,9 @@ def create_planets():
 	"""
 	planets = []
 
-	earth = Planet("Earth", (0, 0, 255), 10, 1, (width // 2, 200), velocity=(1,0))
+	earth = Planet("Earth", BLUE, 10, 1, (width // 2, 200), velocity=(1,0))
 	sun = Planet("Sun", (255, 255, 0), int(696340 // SCALE), 1000, (width // 2, height // 2))
-	mars = Planet("MARS", (255, 0, 0), int(300000 // SCALE), 10, (width // 3, height // 2), velocity=(0,1))
+	mars = Planet("MARS", RED, int(300000 // SCALE), 10, (width // 3, height // 2), velocity=(0,1))
 
 	planets.append(earth)
 	planets.append(mars)
@@ -38,14 +49,14 @@ def main():
 	fpsClock = pygame.time.Clock()
 	screen = pygame.display.set_mode((width, height))
 
-	font = pygame.font.SysFont("Arial", 32)
+	font = pygame.font.SysFont("Arial", FONT_SIZE)
 
 	planets = create_planets()
 	highlighted_planet = None
 
 	# Game loop.
 	while True:
-		screen.fill((0, 0, 0))
+		screen.fill(BLACK)
 		
 		for event in pygame.event.get():
 			if event.type == QUIT:
@@ -72,26 +83,39 @@ def main():
 		# Draw all planets and relative labels (labels are temporary).
 		for planet in planets:
 			pygame.draw.circle(screen, planet.color, planet.pos.astype(int), planet.radius)
+
+			# Going to draw an highlighted circle around the highlighted planet
+			# And show some information about that planet.
 			if highlighted_planet == planet:
-				pygame.draw.circle(screen, (0, 255, 0), planet.pos.astype(int), planet.radius + 5, 1)
+				pygame.draw.circle(screen, GREEN, planet.pos.astype(int), planet.radius + HIGHLIGHTED_PLANET_CIRCLE_OFFSET, 1)
 
 				name_label = font.render(f"{planet.name}", False, planet.color)
-				mass_label = font.render(f"Mass: {planet.mass}", False, (255, 255, 255))
-				position_label = font.render(f"Position: {str(np.round(planet.pos))}", False, (255, 255, 255))
-				velocity_label = font.render(f"Velocity: {str(np.round(planet.velocity, 2))}", False, (255, 255, 255))
+				mass_label = font.render(f"Mass: {planet.mass}", False, WHITE)
+				position_label = font.render(f"Position: {str(np.round(planet.pos))}", False, WHITE)
+				velocity_label = font.render(f"Velocity: {str(np.round(planet.velocity, 2))}", False, WHITE)
 
 				screen.blit(name_label, (0, 0))
-				screen.blit(mass_label, (0, 34))
-				screen.blit(position_label, (0, 34 * 2))
-				screen.blit(velocity_label, (0, 34 * 3))
+				screen.blit(mass_label, (0, FONT_SIZE))
+				screen.blit(position_label, (0, FONT_SIZE * 2))
+				screen.blit(velocity_label, (0, FONT_SIZE * 3))
 
 		# Update screen.
 		pygame.display.flip()
 		fpsClock.tick(fps)
 
 def handle_left_click(planets):
+	"""
+	This functions handles the left click button of the mouse
+
+	Args:
+		planets (List of planets): The planets list
+
+	Returns:
+		Planet: The planet we clicked on, or None if we haven't clicked on a planet.
+	"""
 	for planet in planets:
 		if planet.clicked_on(np.array(pygame.mouse.get_pos())):
 			return planet
+	
 if __name__ == "__main__":
 	main()
